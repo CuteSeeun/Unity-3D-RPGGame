76822ef707e0d51aa5e0b14cd7860e4e75709299
98Seungmo <151799353+98Seungmo@users.noreply.github.com>
2024-03-28 13:29:30 +0900

    형준이형이 압축해달래요

--- Cos/Assets/LKJ/KJ/2. Scripts/02. DungeonScripts/DungeonInteraction.cs
+++ Cos/Assets/LKJ/KJ/2. Scripts/02. DungeonScripts/DungeonInteraction.cs
@@ -1,7 +1,6 @@
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.UIElements;
 using UnityEngine;
 
 public class DungeonInteraction : MonoBehaviour
